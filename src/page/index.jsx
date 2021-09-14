0109bd862ac3fe9e3ee1daee3a766db008a4ebcb
victor Farias <victorfarias.tv@gmail.com>
2021-09-14 20:02:00 -0300

    Novo slider

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Info from '../components/about/info';
 import Profile from '../components/about/profile';
+import { fundamentos, frontend, backend } from '../data';
 import Backend from '../components/projects/back_projects';
 import Fundamentos from '../components/projects/fund_projects';
 import Frontend from '../components/projects/front_projects';
@@ -26,9 +27,9 @@ class Sobre extends React.Component {
         <section className="container">
           <hr />
           <h2>Projetos</h2>
-          <Fundamentos />
-          <Frontend />
-          <Backend />
+          <Fundamentos slides={fundamentos} />
+          <Frontend slides={frontend} />
+          <Backend slides={backend} />
         </section>
       </main>
     );
